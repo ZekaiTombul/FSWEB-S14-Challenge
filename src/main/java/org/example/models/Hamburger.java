b08c0110ee9f5cedde8452348b32938e6ab850ff
ZekaiTombul <zekaitombul@gmail.com>
2025-03-28 03:51:12 +0300

    IdChange

---
+++ src/main/java/org/example/models/Hamburger.java
@@ -0,0 +1,39 @@
+package org.example.models;
+
+public class Hamburger {
+
+    private String name;
+    private String meat;
+    private double price;
+    private String breadRollType;
+
+    private String addition1Name;
+    private String addition2Name;
+    private String addition3Name;
+    private String addition4Name;
+    private double addition1Price;
+    private double addition2Price;
+    private double addition3Price;
+    private double addition4Price;
+
+    public void addHamburgerAddition1(String addition1Name, double addition1Price){
+
+    }
+
+    public void addHamburgerAddition2(String addition2Name, double addition2Price){
+
+    }
+
+    public void addHamburgerAddition3(String addition3Name, double addition3Price){
+
+    }
+
+    public void addHamburgerAddition4(String addition4Name, double addition4Price){
+
+    }
+
+    public void itemizeHamburger(){
+
+    }
+
+}
